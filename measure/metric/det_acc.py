ccd4c67b62aafb133134944619e0a15df3d32b04
thinhtq <Trinhthinh2171999@gmail.com>
2022-05-25 15:37:32 +0700

    initial

--- measure/metric/det_acc.py
+++ measure/metric/det_acc.py
@@ -111,10 +111,7 @@ class DetAcc:
             else:
                 r = totalMatch / totalTarget
             recall.update(r)
-            if p + r == 0:
-                f = 1. if 2. * p * r == 0. else 0.
-            else:
-                f = 2. * p * r / (p + r)
+            f = 0. if (p + r) == 0. else 2. * p * r / (p + r)
             f1score.update(f)
         self._result.clear()
         return dict(precision=precision.calc(),
